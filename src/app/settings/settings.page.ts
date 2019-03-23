3a75076bea2cfd48414556f624391f7d6755a2c1
javierabao <jbbao@uc.cl>
2019-03-23 19:55:22 -0300

    location fix

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -13,7 +13,6 @@ export class SettingsPage implements OnInit {
   geoLatitude: number;
   geoLongitude: number;
   geoAccuracy:number;
-  geoAddress: string;
  
   watchLocationUpdates:any; 
   loading:any;
@@ -31,58 +30,13 @@ export class SettingsPage implements OnInit {
 }
 
 //Get current coordinates of device
-    getGeolocation(){
-      this.geolocation.getCurrentPosition().then((resp) => {
+  public ionViewWillEnter(): void {
+    this.geolocation.getCurrentPosition().then((resp) => {
         this.geoLatitude = resp.coords.latitude;
         this.geoLongitude = resp.coords.longitude; 
         this.geoAccuracy = resp.coords.accuracy; 
-        this.getGeoencoder(this.geoLatitude,this.geoLongitude);
        }).catch((error) => {
          alert('Error getting location'+ JSON.stringify(error));
        });
-    }
-  
-    //geocoder method to fetch address from coordinates passed as arguments
-    getGeoencoder(latitude,longitude){
-      this.nativeGeocoder.reverseGeocode(latitude, longitude, this.geoencoderOptions)
-      .then((result: NativeGeocoderReverseResult[]) => {
-        this.geoAddress = this.generateAddress(result[0]);
-      })
-      .catch((error: any) => {
-        alert('Error getting location'+ JSON.stringify(error));
-      });
-    }
-  
-    //Return Comma saperated address
-    generateAddress(addressObj){
-        let obj = [];
-        let address = "";
-        for (let key in addressObj) {
-          obj.push(addressObj[key]);
-        }
-        obj.reverse();
-        for (let val in obj) {
-          if(obj[val].length)
-          address += obj[val]+', ';
-        }
-      return address.slice(0, -2);
-    }
-  
-  
-    //Start location update watch
-    watchLocation(){
-      this.isWatching = true;
-      this.watchLocationUpdates = this.geolocation.watchPosition();
-      this.watchLocationUpdates.subscribe((resp) => {
-        this.geoLatitude = resp.coords.latitude;
-        this.geoLongitude = resp.coords.longitude; 
-        this.getGeoencoder(this.geoLatitude,this.geoLongitude);
-      });
-    }
-  
-    //Stop location update watch
-    stopLocationWatch(){
-      this.isWatching = false;
-      this.watchLocationUpdates.unsubscribe();
-    }
+  }
 }
